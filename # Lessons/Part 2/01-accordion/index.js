c30e96b1de0112e8108dee4c5b9e5e21bc95f9bf
Hung Bui <hungbui7690@gmail.com>
2024-08-12 09:24:43 -0600

    Pico CSS Docs

---
+++ # Lessons/Part 2/01-accordion/index.js
@@ -0,0 +1,11 @@
+/*
+  Version Picker
+  - https://picocss.com/docs/version-picker
+  - Many theme colors
+    -> <link rel="stylesheet" href="css/pico.min.css" />
+    -> <link rel="stylesheet" href="css/pico.slate.min.css" />
+    ...
+
+
+
+*/
